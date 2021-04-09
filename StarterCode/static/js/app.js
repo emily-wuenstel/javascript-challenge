07f25c90cdda6e2a8a1312c600f9e5dc76078296
emily-wuenstel <74880899+emily-wuenstel@users.noreply.github.com>
2021-04-09 18:11:21 -0500

    Update app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -20,11 +20,13 @@ function runEnter() {
 
     //Find input and get HTML
     var inputElement = d3.select('#datetime');
+
+    console.log(inputElement);
     
     //Find value property
-    var inputVal = inputElement.propery('value');
+    var inputVal = inputElement.property('value');
 
-    var filteredData = data.filter(datetime => datetime.datetime === inputValue);
+    var filteredData = data.filter(datetime => datetime.datetime === inputVal);
 
     let response = {
         filteredData
@@ -42,7 +44,16 @@ function runEnter() {
 
 function buildTable(d) {
 
+    tbody.html('');
 
+    d.forEach(function(inputData) {
+        var row = tbody.append('tr');
+        Object.entries(inputData).forEach(function([key, value]) {
+            var cell = row.append('td');
+            cell.text(value);
+            });
+    });
 
-    
 }
+
+buildTable(data)
