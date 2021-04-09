bd88abce96382f0af85d5c8e4190c5c65198f097
emily-wuenstel <74880899+emily-wuenstel@users.noreply.github.com>
2021-04-09 18:21:40 -0500

    Update app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -7,6 +7,7 @@ var tbody = d3.select('tbody');
 
 //console.log(data);
 
+//Create action when the button is pressed
 var button = d3.select('#filter-btn')
 
 var form = d3.select('form')
@@ -15,25 +16,28 @@ var form = d3.select('form')
 button.on('click', runEnter);
 form.on('submit', runEnter);
 
-//Complete the function for the form
+//Create runEnter function for when button is pressed
 function runEnter() {
 
-    //Find input and get HTML
+    //Find the data from data and store into element
     var inputElement = d3.select('#datetime');
-
-    console.log(inputElement);
-    
-    //Find value property
+   
+    //Find true value of the field for use later 
     var inputVal = inputElement.property('value');
 
+    //log it out to make sure it's stored correctly 
+    //console.log(inputVal);
+
+    //create filter based on the date found
     var filteredData = data.filter(datetime => datetime.datetime === inputVal);
 
+    //check to see if the data returned is lenght of 0 (no results) or not, and act accordingly
     let response = {
         filteredData
     }
 
     if (response.filteredData.length !== 0) {
-        buildtable(filteredData)
+        buildTable(filteredData)
     }
 
     else {
