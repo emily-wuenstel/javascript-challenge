c526cc77e1dabeb9fde21d7d0342153291b05ed9
emily-wuenstel <74880899+emily-wuenstel@users.noreply.github.com>
2021-04-10 13:54:15 -0500

    Update app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -17,72 +17,85 @@ function runEnter() {
     //Find the data from data and store into element
     var inputElement = d3.select('#datetime');
     var inputElement2 = d3.select('#city');
-
-
+    var inputElement3 = d3.select('#state');
+    var inputElement4 = d3.select('#country');
 
     if (inputElement.property('value') !== '') {
-        var inputDate = inputElement.property('value');
+        var inputDate = inputElement.property('value')
     }
-    else {
+    else if (inputElement2.property('value') !== '') {
         var inputCity = inputElement2.property('value')
         var inputDate = ''
     }
-
-    console.log(inputCity)
-    console.log(inputDate)
+    else if (inputElement3.property('value') !== '') {
+        var inputState = inputElement3.property('value')
+        var inputCity = ''
+        var inputDate = ''
+    }
+    else {
+        var inputCountry = inputElement4.property('value')
+        var inputState = ''
+        var inputCity = ''
+        var inputDate = ''
+    }
+    
+//-------------------------------------------------------------------------------------------------------------------------------------------
+//Figure out what values to pass through to the filter function
 
     if (inputDate !== '') {
-        //clear input 
-        d3.select('#datetime').node().value = ''
-        
-        // create filter based on the date found
-        var filteredData = data.filter(datetime => datetime.datetime === inputDate);
-
-        //check to see if the data returned is lenght of 0 (no results) or not, and act accordingly
-         let response = {
-             filteredData
-         }
-
-         //if the length is not zero, build the table based on the reults from filter
-         if (response.filteredData.length !== 0) {
-             buildTable(filteredData)
-         }
-
-         //if the length is zero, inform the user no results were available and to try again
-         else {
-             tbody.html('')
-             tbody.append("tr").append("td").text("No results available, please search again");
-         }
-     }
-        
+        filterData(inputDate, 'date')
+    }   
+    else if (inputCity !== '') {
+        filterData(inputCity, 'city')
+    }
+    else if (inputState !== '') {
+        filterData(inputState, 'state')
+    }
     else {
-        //clear input
-        d3.select('#city').node().value = ''
-
-        //create filter based on the city found
-         var filteredData = data.filter(city => city.city === inputCity);
-
-        //check to see if the data returned is lenght of 0 (no results) or not, and act accordingly
-         let response = {
-             filteredData
-         }
-
-         //if the length is not zero, build the table based on the reults from filter
-         if (response.filteredData.length !== 0) {
-             buildTable(filteredData)
-         }
-
-         //if the length is zero, inform the user no results were available and to try again
-         else {
-             tbody.html('')
-             tbody.append("tr").append("td").text("No results available, please search again");
-         }
-     }
+        filterData(inputCountry, 'country')
+    }
         
 }
 //-------------------------------------------------------------------------------------------------------------------------------------------
 //create data function which will be called depending on which data variant is filled in 
+function filterData(input, type) {
 
+    console.log(input)
+    console.log(type)
+
+    if (type === 'city') {
+        d3.select('#city').node().value = ''
+        var filteredData = data.filter(city => city.city === input)
+    }
+    if (type === 'datetime'){
+        d3.select('#datetime').node().value = ''
+        var filteredData = data.filter(datetime => datetime.datetime === input)
+    }
+    if (type === 'state'){
+        d3.select('#state').node().value = ''
+        var filteredData = data.filter(state => state.state === input)
+    }
+    if (type === 'country') {
+        d3.select('#country').node().value = ''
+        var filteredData = data.filter(country => country.country === input)   
+    }
+
+
+    //Get the response data and build the table
+    let response = {
+        filteredData
+    }
+
+    if (response.filteredData.length !== 0) {
+        buildTable(filteredData)
+    }
+
+    else {
+        tbody.html('')
+        tbody.append("tr").append("td").text("No results available, please search again");  
+    }
+
+}
 
 
 
@@ -119,4 +132,3 @@ resetButton.on('click', runReset);
 function runReset() {
     buildTable(data)
 }
-//-------------------------------------------------------------------------------------------------------------------------------------------
