d20114780111de7e60699fd56eb81e37b63a1f98
emily-wuenstel <74880899+emily-wuenstel@users.noreply.github.com>
2021-04-10 13:28:55 -0500

    Update app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,14 +1,10 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
 var tbody = d3.select('tbody');
-
-//console.log(data);
 
 //Create action when the button is pressed
 var button = d3.select('#filter-btn')
-
 var form = d3.select('form')
 
 //Button clicking action
@@ -20,33 +16,80 @@ function runEnter() {
 
     //Find the data from data and store into element
     var inputElement = d3.select('#datetime');
-   
-    //Find true value of the field for use later 
-    var inputVal = inputElement.property('value');
+    var inputElement2 = d3.select('#city');
 
-    //log it out to make sure it's stored correctly 
-    //console.log(inputVal);
 
-    //create filter based on the date found
-    var filteredData = data.filter(datetime => datetime.datetime === inputVal);
 
-    //check to see if the data returned is lenght of 0 (no results) or not, and act accordingly
-    let response = {
-        filteredData
+    if (inputElement.property('value') !== '') {
+        var inputDate = inputElement.property('value');
     }
-
-    //if the length is not zero, build the table based on the reults from filter
-    if (response.filteredData.length !== 0) {
-        buildTable(filteredData)
-    }
-
-    //if the length is zero, inform the user no results were available and to try again
     else {
-        tbody.html('')
-        tbody.append("tr").append("td").text("No results available, please search again");
+        var inputCity = inputElement2.property('value')
+        var inputDate = ''
     }
-}
 
+    console.log(inputCity)
+    console.log(inputDate)
+
+    if (inputDate !== '') {
+        //clear input 
+        d3.select('#datetime').node().value = ''
+        
+        // create filter based on the date found
+        var filteredData = data.filter(datetime => datetime.datetime === inputDate);
+
+        //check to see if the data returned is lenght of 0 (no results) or not, and act accordingly
+         let response = {
+             filteredData
+         }
+
+         //if the length is not zero, build the table based on the reults from filter
+         if (response.filteredData.length !== 0) {
+             buildTable(filteredData)
+         }
+
+         //if the length is zero, inform the user no results were available and to try again
+         else {
+             tbody.html('')
+             tbody.append("tr").append("td").text("No results available, please search again");
+         }
+     }
+        
+    else {
+        //clear input
+        d3.select('#city').node().value = ''
+
+        //create filter based on the city found
+         var filteredData = data.filter(city => city.city === inputCity);
+
+        //check to see if the data returned is lenght of 0 (no results) or not, and act accordingly
+         let response = {
+             filteredData
+         }
+
+         //if the length is not zero, build the table based on the reults from filter
+         if (response.filteredData.length !== 0) {
+             buildTable(filteredData)
+         }
+
+         //if the length is zero, inform the user no results were available and to try again
+         else {
+             tbody.html('')
+             tbody.append("tr").append("td").text("No results available, please search again");
+         }
+     }
+        
+}
+//-------------------------------------------------------------------------------------------------------------------------------------------
+//create data function which will be called depending on which data variant is filled in 
+
+
+
+
+
+
+
+//-------------------------------------------------------------------------------------------------------------------------------------------
 //buildtable function which is called to build the table when results are found (called in line 41)
 function buildTable(d) {
 
@@ -62,5 +105,18 @@ function buildTable(d) {
 
 }
 
-//build entire table
+
+//-------------------------------------------------------------------------------------------------------------------------------------------
+//build entire table as default
 buildTable(data)
+
+//-------------------------------------------------------------------------------------------------------------------------------------------
+//Reset Button Functionality 
+var resetButton = d3.select('#reset-btn')
+
+resetButton.on('click', runReset);
+
+function runReset() {
+    buildTable(data)
+}
+//-------------------------------------------------------------------------------------------------------------------------------------------
